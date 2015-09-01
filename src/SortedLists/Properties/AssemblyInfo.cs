0f7f60767c16dff322b5adb66728fa7deb17ba70
Mikkel Riise Lund <lund.mikkel@gmail.com>
2015-09-01 10:28:48 +1000

    Initial commit.

---
+++ src/SortedLists/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Orcomp development team">
+//   Copyright (c) 2012 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+#region using...
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: InternalsVisibleTo("SortedLists")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SortedLists")]
+[assembly: AssemblyDescription("SortedLists")]
+[assembly: AssemblyProduct("SortedLists")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// For version information see the centralized SolutionAssemblyInfo.cs
+// Do not comment out the following lines:
+// [assembly: AssemblyVersion("x.x.x.x")]
+// [assembly: AssemblyFileVersion("x.x.x.x")]
+// [assembly: AssemblyInformationalVersion("x.x.x.x")]
