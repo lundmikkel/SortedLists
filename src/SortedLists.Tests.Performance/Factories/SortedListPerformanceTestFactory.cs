9963a92098d8f02abd73f5b50afaf7a470af7486
Mikkel Riise Lund <lund.mikkel@gmail.com>
2015-09-09 12:51:59 +1000

    Adds add tests inserting in sorted order.

--- src/SortedLists.Tests.Performance/Factories/SortedListPerformanceTestFactory.cs
+++ src/SortedLists.Tests.Performance/Factories/SortedListPerformanceTestFactory.cs
@@ -16,12 +16,15 @@
             get
             {
                 return new[]{
-                    typeof (SortedList<>),
-                    typeof (SortedArrayList<>),
-                    typeof (SortedArray<>),
-                    typeof (SortedGapBuffer<>),
-                    typeof (RedBlackBinarySearchTree<>),
+                    //typeof (SortedList<>),
+                    //typeof (SortedArrayList<>),
+                    //typeof (SortedArray<>),
+                    //typeof (SortedGapBuffer<>),
+                    //typeof (RedBlackBinarySearchTree<>),
+                    //typeof (DoublyLinkedRedBlackBinarySearchTree<>),
+                    typeof (BTree<>),
                     typeof (SortedSplitList<>),
+                    //typeof (SortedSplitGapBuffer<>),
                 };
             }
         }
@@ -32,8 +35,9 @@
             {
                 //return new[] { 1 << 5, 1 << 6, 1 << 7, 1 << 8, 1 << 9, 1 << 10, 1 << 11 };
                 //return new[] { 5 * 1, 100 * 1, 200 * 1, 400 * 1, 800 * 1, 1600 * 1 };
-                return new[] { 5 * H, 100 * H, 200 * H, 400 * H, 800 * H, 1600 * H };
-                return new[] { 5 * K, 100 * K, 200 * K, 400 * K, 800 * K, 1600 * K };
+                //return new[] { 5 * H, 100 * H, 200 * H, 400 * H, 800 * H, 1600 * H };
+                //return new[] { 5 * K, 100 * K, 200 * K, 400 * K, 800 * K, 1600 * K };
+                return new[] { 5 * K, 100 * K, 200 * K, 400 * K, 800 * K, 1600 * K, 3200 * K, 6400 * K, 12800 * K };
             }
         }
 
@@ -64,6 +68,78 @@
                    select new SortedListPerformanceTestCaseConfiguration<int>
                    {
                        TestName = "RandomIntAdd",
+                       TargetImplementationType = implementationType,
+                       Identifier = string.Format("{0}", implementationType.GetFriendlyName()), //, parameters.FirstOrDefault()),
+                       Size = size,
+                       Prepare = prepare,
+                       Run = run,
+                       IsReusable = false
+                   };
+        }
+
+        public IEnumerable<SortedListPerformanceTestCaseConfiguration<int>> AscendingIntAdd()
+        {
+            // Issue in NUnit: even this method is called _earlier_ than TestFixtureSetup....
+            // so we can not call GetImplementations here, because FindImplementatins was not called yet :-(
+
+            return from implementationType in ImplementationTypes
+                   //from parameters in GetParameters(implementationType)
+                   from size in Sizes
+                   let prepare = new Action<IPerformanceTestCaseConfiguration>(c =>
+                   {
+                       var config = (SortedListPerformanceTestCaseConfiguration<int>) c;
+                       GenerateAscendingOrderUniqueInts(size, config);
+                       config.Target = CreateSortedList<int>(implementationType); //, parameters);
+                   })
+                   let run = new Action<IPerformanceTestCaseConfiguration>(c =>
+                   {
+                       var config = (SortedListPerformanceTestCaseConfiguration<int>) c;
+                       var target = config.Target;
+
+                       for (var i = 0; i < size; i++)
+                       {
+                           target.Add(config.RandomItems[i]);
+                       }
+                   })
+                   select new SortedListPerformanceTestCaseConfiguration<int>
+                   {
+                       TestName = "AscendingIntAdd",
+                       TargetImplementationType = implementationType,
+                       Identifier = string.Format("{0}", implementationType.GetFriendlyName()), //, parameters.FirstOrDefault()),
+                       Size = size,
+                       Prepare = prepare,
+                       Run = run,
+                       IsReusable = false
+                   };
+        }
+
+        public IEnumerable<SortedListPerformanceTestCaseConfiguration<int>> DescendingIntAdd()
+        {
+            // Issue in NUnit: even this method is called _earlier_ than TestFixtureSetup....
+            // so we can not call GetImplementations here, because FindImplementatins was not called yet :-(
+
+            return from implementationType in ImplementationTypes
+                   //from parameters in GetParameters(implementationType)
+                   from size in Sizes
+                   let prepare = new Action<IPerformanceTestCaseConfiguration>(c =>
+                   {
+                       var config = (SortedListPerformanceTestCaseConfiguration<int>) c;
+                       GenerateDescendingOrderUniqueInts(size, config);
+                       config.Target = CreateSortedList<int>(implementationType); //, parameters);
+                   })
+                   let run = new Action<IPerformanceTestCaseConfiguration>(c =>
+                   {
+                       var config = (SortedListPerformanceTestCaseConfiguration<int>) c;
+                       var target = config.Target;
+
+                       for (var i = 0; i < size; i++)
+                       {
+                           target.Add(config.RandomItems[i]);
+                       }
+                   })
+                   select new SortedListPerformanceTestCaseConfiguration<int>
+                   {
+                       TestName = "DescendingIntAdd",
                        TargetImplementationType = implementationType,
                        Identifier = string.Format("{0}", implementationType.GetFriendlyName()), //, parameters.FirstOrDefault()),
                        Size = size,
@@ -134,6 +210,20 @@
             var list = new C5.ArrayList<int>(size);
             list.AddAll(Enumerable.Range(0, size));
             list.Shuffle();
+            config.RandomItems = list.ToArray();
+        }
+
+        private void GenerateAscendingOrderUniqueInts(int size, SortedListPerformanceTestCaseConfiguration<int> config)
+        {
+            var list = new C5.ArrayList<int>(size);
+            list.AddAll(Enumerable.Range(0, size));
+            config.RandomItems = list.ToArray();
+        }
+
+        private void GenerateDescendingOrderUniqueInts(int size, SortedListPerformanceTestCaseConfiguration<int> config)
+        {
+            var list = new C5.ArrayList<int>(size);
+            list.AddAll(Enumerable.Range(0, size).Reverse());
             config.RandomItems = list.ToArray();
         }
 
--- src/SortedLists.Tests.Performance/InterfaceTests/SortedListPerformanceTest.cs
+++ src/SortedLists.Tests.Performance/InterfaceTests/SortedListPerformanceTest.cs
@@ -18,7 +18,19 @@
             config.Benchmark(config.TestName, config.Size, 3);
         }
 
-        [Test, TestCaseSource(typeof(SortedListPerformanceTestFactory), "RandomStringAdd")]
+        [Test, TestCaseSource(typeof(SortedListPerformanceTestFactory), "AscendingIntAdd")]
+        public void AscendingIntAdd(SortedListPerformanceTestCaseConfiguration<int> config)
+        {
+            config.Benchmark(config.TestName, config.Size, 3);
+        }
+
+        [Test, TestCaseSource(typeof(SortedListPerformanceTestFactory), "DescendingIntAdd")]
+        public void DescendingIntAdd(SortedListPerformanceTestCaseConfiguration<int> config)
+        {
+            config.Benchmark(config.TestName, config.Size, 3);
+        }
+
+        [Test, Ignore, TestCaseSource(typeof(SortedListPerformanceTestFactory), "RandomStringAdd")]
         public void RandomStringAdd(SortedListPerformanceTestCaseConfiguration<string> config)
         {
             config.Benchmark(config.TestName, config.Size, 3);
